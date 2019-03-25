664f0351f996dc5ec59532ab32afe9733820e270
Nalecy <656177313@qq.com>
2019-03-26 00:50:35 +0800

    no message

---
+++ src/com/Nalecy/www/service/HotelService.java
@@ -0,0 +1,18 @@
+package com.Nalecy.www.service;
+
+import com.Nalecy.www.po.Administrator;
+import com.Nalecy.www.po.Customer;
+import com.Nalecy.www.po.HotelAdmin;
+import com.Nalecy.www.po.Person;
+
+public class HotelService {
+    public static String getPassword(String userName){
+        return "123456";
+    }
+    public static Person searchPerson(Integer personId){
+        return new Customer();
+    }
+    public static Integer getPersonID(String userName){
+        return null;
+    }
+}
