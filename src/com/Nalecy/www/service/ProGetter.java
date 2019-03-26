fdff08448cf805ea078e63bd592fdf58451b4ffe
Nalecy <656177313@qq.com>
2019-03-26 13:48:28 +0800

    feat(service) : 增加了专门获取配置文件的单例类

---
+++ src/com/Nalecy/www/service/ProGetter.java
@@ -0,0 +1,29 @@
+package com.Nalecy.www.service;
+
+import com.Nalecy.www.Runner;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ProGetter {
+    private static ProGetter ourInstance = new ProGetter();
+    private static Properties pro;
+
+    public static ProGetter getInstance() {
+        return ourInstance;
+    }
+
+    private ProGetter() {
+        pro = new Properties();
+        String path = Runner.class.getResource("HotelSystem.properties").getPath();
+        try {
+            pro.load(new FileReader(path));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public String get(String key){
+        return pro.getProperty(key);
+    }
+}
