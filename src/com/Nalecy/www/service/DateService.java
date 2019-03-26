d0b08b4144b7a727e0d36cae01829c26a241ce21
Nalecy <656177313@qq.com>
2019-03-26 17:00:02 +0800

    feat(service) : 增加了日期系统

---
+++ src/com/Nalecy/www/service/DateService.java
@@ -0,0 +1,27 @@
+package com.Nalecy.www.service;
+
+
+//import java.text.SimpleDateFormat;
+import java.sql.Date;
+
+public class DateService {
+    private static Date currentDate;
+
+    private static DateService ourInstance = new DateService();
+
+    public static DateService getInstance() {
+        return ourInstance;
+    }
+
+    private DateService() {
+        //获取数据库时间
+        currentDate = new Date(0);
+    }
+    public String getCurrentDate(){
+        return currentDate.toString();
+        // return new SimpleDateFormat("yyyy年MM月dd日").format(currentDate);
+    }
+    public void incrDate(){
+        currentDate.setTime(currentDate.getTime() + 3600*1000*24);
+    }
+}
