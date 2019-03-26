ac69beccafb803cd1719a94f6870d5fe794c07b5
Nalecy <656177313@qq.com>
2019-03-26 14:38:25 +0800

    no message

--- src/com/Nalecy/www/service/HotelService.java
+++ src/com/Nalecy/www/service/HotelService.java
@@ -1,24 +1,19 @@
 package com.Nalecy.www.service;
 
-import com.Nalecy.www.po.Administrator;
-import com.Nalecy.www.po.Customer;
-import com.Nalecy.www.po.HotelAdmin;
-import com.Nalecy.www.po.Person;
-
 public class HotelService {
-    public static String getPassword(String userName){
-        return "123456";
-    }
-    public static Person searchPerson(Integer personId){
-        return new Customer();
-    }
-    public static Integer getPersonID(String userName){
-        return null;
+    private String currentUser = null;
+    private static HotelService ourInstance = new HotelService();
+    public static HotelService getInstance() {
+        return ourInstance;
     }
 
-    public static boolean saveUser(String userName) {
-        Integer id = getPersonID(userName);
-        //save(id, userName);
-        return true;
+    private HotelService() {
+    }
+
+    public void setCurrentUser(String userName){
+        currentUser = userName;
+    }
+    public String getCurrentUser() {
+        return currentUser;
     }
 }
