de83bcb493a852d1ab3a594e32ee92efdf8fe659
Nalecy <656177313@qq.com>
2019-03-26 18:08:21 +0800

    feat(view) : 添加了取消保存密码的选项 feat(view) : 完成了顾客菜单的显示

--- src/com/Nalecy/www/view/CustomerMenu.java
+++ src/com/Nalecy/www/view/CustomerMenu.java
@@ -2,12 +2,50 @@ package com.Nalecy.www.view;
 
 import com.Nalecy.www.service.DateService;
 import com.Nalecy.www.service.HotelService;
+import com.Nalecy.www.service.PersonService;
+import com.Nalecy.www.view.customerMenu.HotelListMenu;
+import com.Nalecy.www.view.customerMenu.OrderMenu;
+import com.Nalecy.www.view.customerMenu.PsnlInfoMenu;
+
+import java.util.regex.Pattern;
 
 public class CustomerMenu extends Menu {
     @Override
     public void show() {
-        System.out.println("\t\t\t今天是"+ DateService.getInstance().getCurrentDate().toString());
+        while (true) {
+            printMenu();
+            String choice = in.next();
+            if(choice.equals("0"))break;
+            if(Pattern.matches("^[1289]$",choice)){
+                Integer c = new Integer(choice);
+                switch (c){
+                    case 1:showNextMenu(new HotelListMenu());break;
+                    case 2:showNextMenu(new OrderMenu());break;
+                    case 8:cancelLogin();break;
+                    case 9:showNextMenu(new PsnlInfoMenu());break;
+                }
+            }else {
+                System.out.println("请检查输入");
+            }
+        }
+    }
+
+    private void printMenu() {
+        System.out.println("\t\t\t今天是"+ DateService.getInstance().getCurrentDate());
         System.out.println("欢迎,用户名为"+ HotelService.getInstance().getCurrentUser()+"的顾客。");
-        System.out.println("");
+        System.out.println("您想要：");
+        System.out.println("1、查看酒店");
+        System.out.println("2、查看订单");
+        System.out.println("8、取消自动登录");
+        System.out.println("9、修改个人信息");
+        System.out.println("0、退出至登录界面");
+    }
+
+    private void cancelLogin() {
+        if (PersonService.cancelLogin(HotelService.getInstance().getCurrentUser())) System.out.println("取消成功");
+        else System.out.println("删除失败");
+    }
+    private void showNextMenu(Menu menu) {
+        menu.show();
     }
 }
