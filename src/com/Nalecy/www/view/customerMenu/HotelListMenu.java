b96cd0a95c66e3817b6b769cd5ec870fbcaa0587
Nalecy <656177313@qq.com>
2019-03-28 01:24:23 +0800

    feat(view) : 完成了酒店列表的界面

--- src/com/Nalecy/www/view/customerMenu/HotelListMenu.java
+++ src/com/Nalecy/www/view/customerMenu/HotelListMenu.java
@@ -1,11 +1,41 @@
 package com.Nalecy.www.view.customerMenu;
 
+import com.Nalecy.www.po.Hotel;
+import com.Nalecy.www.service.HotelService;
 import com.Nalecy.www.view.Menu;
+import com.Nalecy.www.view.customerMenu.submenu.HotelMenu;
+
+import java.util.ArrayList;
 
 public class HotelListMenu  extends Menu {
 
     @Override
     public void show() {
-        System.out.println("这里是查看酒店菜单");
+        ArrayList<Hotel> hotelList;
+        Hotel hotel;
+        hotelList = HotelService.getInstance().getHotelList();
+        while (true) {
+            hotel = null;
+            System.out.println("下列是酒店列表 (输入0返回)");
+            for (Hotel h : hotelList) {
+                System.out.println(h.toString());
+            }
+            System.out.println("您想进入那个酒店(请输入名字)：");
+            String name = in.nextLine();
+            if (name.equals("0"))break;                 //输入0退出
+            for (Hotel h : hotelList) {
+                if(h.getName().equals(name)){
+                    hotel = h;
+                    break;
+                }
+            }
+            if(hotel == null){
+                System.out.println("无该酒店！");
+            }else {
+                HotelService.getInstance().setCurrentHotel(hotel);
+                showNextMenu(new HotelMenu());
+            }
+        }
+
     }
 }
