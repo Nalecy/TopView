97784830c7748d9045da640e23e09cb2ff4ad3cd
Nalecy <656177313@qq.com>
2019-03-28 01:25:05 +0800

    no message

---
+++ src/com/Nalecy/www/view/customerMenu/submenu/RoonMenu.java
@@ -0,0 +1,10 @@
+package com.Nalecy.www.view.customerMenu.submenu;
+
+import com.Nalecy.www.view.Menu;
+
+public class RoonMenu extends Menu {
+    @Override
+    public void show() {
+        System.out.println("这是房间菜单");
+    }
+}
