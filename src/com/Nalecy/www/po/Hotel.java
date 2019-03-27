9d9a67acefc6b79368458f8e1ddef4b524758742
Nalecy <656177313@qq.com>
2019-03-28 01:19:17 +0800

    feat(po) : 完善了toString方法

--- src/com/Nalecy/www/po/Hotel.java
+++ src/com/Nalecy/www/po/Hotel.java
@@ -1,22 +1,52 @@
 package com.Nalecy.www.po;
 
+import java.text.DecimalFormat;
+
 public class Hotel {
     private Integer id;
+    private String name;
     private Integer star;
     private Double score;
+    private Integer numOfScore;
     private String description;
 
+    public Hotel() {
+    }
 
+    public Hotel(Integer id, String name, Integer star, Double score, Integer numOfScore, String description) {
+        this.id = id;
+        this.name = name;
+        this.star = star;
+        this.score = score;
+        this.numOfScore = numOfScore;
+        this.description = description;
+    }
 
+    public void updateScore(Integer score){
+        this.score = (this.score * numOfScore + score)/(++numOfScore);
+    }
 
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder(name+"，");
+        for(int i = 0; i<star ; i++) sb.append("★");
+        sb.append("级酒店 ，综合评分："+getScore().toString()+"总评分人数"+numOfScore+"详细介绍： "+description);
+        return sb.toString();
+    }
 
-
+    public Integer getNumOfScore() { return numOfScore; }
+    public void setNumOfScore(Integer numOfScore) {this.numOfScore = numOfScore; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
     public Integer getId() { return id; }
     public void setId(Integer id) { this.id = id; }
     public Integer getStar() { return star; }
     public void setStar(Integer star) { this.star = star; }
-    public Double getScore() { return score; }
-    public void setScore(Double score) { this.score = score; }
+    public Double getScore() {
+        Double s = score;
+        new DecimalFormat("#.00").format(s);
+        return s;
+    }
     public String getDescription() { return description; }
     public void setDescription(String description) { this.description = description; }
 }
--- src/com/Nalecy/www/po/Room.java
+++ src/com/Nalecy/www/po/Room.java
@@ -1,24 +1,49 @@
 package com.Nalecy.www.po;
 
+import com.Nalecy.www.constantClass.*;
+
+
 public class Room {
     private Integer id;
     private Integer type;
-    private String area;
-    private String bedWidth;
-    private String price;
+    private Integer area;
+    private Integer bedWidth;
+    private Integer price;
     private Integer hotelID;
 
+    public Room() {
+    }
+
+    public Room(Integer id, Integer type, Integer area, Integer bedWidth, Integer price, Integer hotelID) {
+        this.id = id;
+        this.type = type;
+        this.area = area;
+        this.bedWidth = bedWidth;
+        this.price = price;
+        this.hotelID = hotelID;
+    }
+
+    @Override
+    public String toString() {
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("房间-- 类型：");
+        if(type.equals(RoomType.NORMAL))sb.append("普通");
+        else if(type.equals(RoomType.ADVANCED))sb.append("高级");
+        sb.append("，面积："+area+"平，床宽："+bedWidth+"cm，价格："+price+"元");
+        return sb.toString();
+    }
 
     public Integer getId() { return id; }
     public void setId(Integer id) { this.id = id; }
     public Integer getType() { return type; }
     public void setType(Integer type) { this.type = type; }
-    public String getArea() { return area; }
-    public void setArea(String area) { this.area = area; }
-    public String getBedWidth() { return bedWidth; }
-    public void setBedWidth(String bedWidth) { this.bedWidth = bedWidth; }
-    public String getPrice() { return price; }
-    public void setPrice(String price) { this.price = price; }
+    public Integer getArea() { return area; }
+    public void setArea(Integer area) { this.area = area; }
+    public Integer getBedWidth() { return bedWidth; }
+    public void setBedWidth(Integer bedWidth) { this.bedWidth = bedWidth; }
+    public Integer getPrice() { return price; }
+    public void setPrice(Integer price) { this.price = price; }
     public Integer getHotelID() { return hotelID; }
     public void setHotelID(Integer hotelID) { this.hotelID = hotelID; }
 }
