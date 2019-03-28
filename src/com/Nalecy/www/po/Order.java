c824a3f0d710955325bb512b80b7ebe42c7e15a5
Nalecy <656177313@qq.com>
2019-03-28 23:45:41 +0800

    feat(po) : 增加了一个属性并重写了toString

--- src/com/Nalecy/www/po/Order.java
+++ src/com/Nalecy/www/po/Order.java
@@ -1,17 +1,41 @@
 package com.Nalecy.www.po;
 
-import java.util.Date;
+import com.Nalecy.www.constantClass.RoomPeriod;
+import com.Nalecy.www.constantClass.RoomType;
+
+import java.sql.Date;
 
 public class Order {
     private Integer id;
+    private String userName;
     private Date date;
     private Integer roomPeriod;
     private Integer hotelID;
     private Integer roomID;
 
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("用户名："+userName+"，预约时间："+date+"预定时间段：");
+        if(roomPeriod.equals(RoomPeriod.MORRNING))sb.append("早上");
+        else if(roomPeriod.equals(RoomPeriod.AFTERNOON))sb.append("下午");
+        else if(roomPeriod.equals(RoomPeriod.NIGHT))sb.append("晚上");
+        return sb.toString();
+    }
 
+    public Order(String userName, Date date, Integer roomPeriod, Integer hotelID, Integer roomID) {
+        this.userName = userName;
+        this.date = date;
+        this.roomPeriod = roomPeriod;
+        this.hotelID = hotelID;
+        this.roomID = roomID;
+    }
 
+    public Order() {
+    }
 
+    public String getUserName() { return userName; }
+    public void setUserName(String userName) { this.userName = userName; }
     public Integer getId() { return id; }
     public void setId(Integer id) { this.id = id; }
     public Date getDate() { return date; }
