c555c36bbe8eb531c52519093c2a4b30719c4e25
Nalecy <656177313@qq.com>
2019-03-28 23:41:37 +0800

    feat(constantClass) : 增加了相对预定时间的3个常量

--- src/com/Nalecy/www/constantClass/RoomPeriod.java
+++ src/com/Nalecy/www/constantClass/RoomPeriod.java
@@ -4,4 +4,7 @@ public interface  RoomPeriod {
     Integer MORRNING = 1;
     Integer AFTERNOON = 2;
     Integer NIGHT = 3;
+    Integer ONE_DAY_LATER = 1;
+    Integer TWO_DAY_LATER = 2;
+    Integer THREE_DAY_LATER = 3;
 }
