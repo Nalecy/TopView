c0e775124984af4189ddf23975e22518957e6ddc
Nalecy <656177313@qq.com>
2019-03-28 23:46:41 +0800

    feat(service) : 增加了方便的获取年月日以及增加天数的方法

--- src/com/Nalecy/www/service/DateService.java
+++ src/com/Nalecy/www/service/DateService.java
@@ -3,10 +3,11 @@ package com.Nalecy.www.service;
 
 //import java.text.SimpleDateFormat;
 import java.sql.Date;
+import java.util.Calendar;
 
 public class DateService {
     private static Date currentDate;
-
+    private static Calendar calendar ;
     private static DateService ourInstance;
 
     public static DateService getInstance() {
@@ -17,12 +18,32 @@ public class DateService {
     private DateService() {
         //获取数据库时间
         currentDate = new Date(0);
+        calendar = Calendar.getInstance();
+        calendar.setTime(currentDate);
     }
-    public String getCurrentDate(){
-        return currentDate.toString();
+    public Integer getYear(){
+        return calendar.get(Calendar.YEAR);
+    }
+    public Integer getMonth(){
+        return calendar.get(Calendar.MONTH) + 1;
+    }
+    public Integer getDay(){
+        return calendar.get(Calendar.DAY_OF_MONTH);
+    }
+
+    public Date getCurrentDate(){
+        return currentDate;
         // return new SimpleDateFormat("yyyy年MM月dd日").format(currentDate);
     }
     public void incrDate(){
         currentDate.setTime(currentDate.getTime() + 3600*1000*24);
+    }
+
+    public void initCalendar(){
+        calendar.setTime(currentDate);
+    }
+
+    public void addDay(Integer i) {
+        calendar.add(Calendar.DAY_OF_MONTH,i);
     }
 }
