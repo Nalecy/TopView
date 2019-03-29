dfeaa2f31464b963e2783abc1890fceee63ee682
Nalecy <656177313@qq.com>
2019-03-30 00:19:48 +0800

    feat(po) : 增加了酒管的便捷构造方法

--- src/com/Nalecy/www/po/HotelAdmin.java
+++ src/com/Nalecy/www/po/HotelAdmin.java
@@ -6,6 +6,12 @@ public class HotelAdmin extends Person{
         permission = 2;
     }
 
+    public HotelAdmin(String userName, String password, String telephone, String idNumber, Integer hotelID) {
+        super(userName, password, telephone, idNumber);
+        this.hotelID = hotelID;
+        permission = 2;
+    }
+
     public Integer getHotelID() { return hotelID; }
     public void setHotelID(Integer hotelID) { this.hotelID = hotelID; }
 }
