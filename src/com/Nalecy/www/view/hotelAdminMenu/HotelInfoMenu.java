a753bd1327c10c0876b7d5f21abdf482256901d9
Nalecy <656177313@qq.com>
2019-03-30 00:22:32 +0800

    feat(view) : 完成了酒管菜单的子界面：查看本酒店信息界面

---
+++ src/com/Nalecy/www/view/hotelAdminMenu/HotelInfoMenu.java
@@ -0,0 +1,19 @@
+package com.Nalecy.www.view.hotelAdminMenu;
+
+import com.Nalecy.www.po.Hotel;
+import com.Nalecy.www.po.HotelAdmin;
+import com.Nalecy.www.service.HotelService;
+import com.Nalecy.www.service.PersonService;
+import com.Nalecy.www.view.Menu;
+
+public class HotelInfoMenu extends Menu {
+    @Override
+    public void show() {
+        HotelAdmin ha = (HotelAdmin) PersonService.getInstance().searchPerson(HotelService.getInstance().getCurrentUser());
+        Hotel hotel = HotelService.getInstance().getHotelById(ha.getHotelID());
+        System.out.println("下列是您任职的酒店信息：");
+        System.out.println(hotel.toString());
+        System.out.println("输入任意值返回......");
+        in.nextLine();
+    }
+}
