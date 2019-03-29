e63b27e6152de7b7207e2676764f8d11bd25af0b
Nalecy <656177313@qq.com>
2019-03-30 00:22:38 +0800

    no message

---
+++ src/com/Nalecy/www/view/hotelAdminMenu/OrderMenu.java
@@ -0,0 +1,11 @@
+package com.Nalecy.www.view.hotelAdminMenu;
+
+import com.Nalecy.www.view.Menu;
+
+public class OrderMenu extends Menu {
+    @Override
+    public void show() {
+        System.out.println("这里是酒店订单菜单");
+
+    }
+}
