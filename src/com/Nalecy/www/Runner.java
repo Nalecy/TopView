c2d5e98790e1d28869e33c158e66b7a18482ff87
Nalecy <656177313@qq.com>
2019-04-07 13:43:44 +0800

    feat(view) : 将启动模式切换为视窗模式启动

--- src/com/Nalecy/www/Runner.java
+++ src/com/Nalecy/www/Runner.java
@@ -1,24 +1,28 @@
 package com.Nalecy.www;
 
 import com.Nalecy.www.view.LoginMenu;
+import com.Nalecy.www.view.LoginView;
 import com.Nalecy.www.view.Menu;
+import javafx.application.Application;
+import javafx.stage.Stage;
 
-public class Runner {
-    private static Runner ourInstance = new Runner();
+public class Runner extends Application {
+   /* private static Runner ourInstance = new Runner();
 
     public static Runner getInstance() {
         return ourInstance;
     }
 
     private Runner() {
-    }
+    }*/
 
     public static void main(String[] args) {
-        Runner.getInstance().start();
+        launch(args);
     }
 
-    private void start() {
-        showMenu(new LoginMenu());
+    public void start(Stage a) {
+        LoginView.display();
+        //showMenu(new LoginMenu());
     }
     private void showMenu(Menu m){
         m.show();
