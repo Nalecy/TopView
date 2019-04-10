04e73daaa0f44028da297328d96c59d8a879a185
Nalecy <656177313@qq.com>
2019-04-10 15:21:37 +0800

    feat(dao) :完成了从数据库获取密码的功能

--- src/com/Nalecy/www/dao/UserDao.java
+++ src/com/Nalecy/www/dao/UserDao.java
@@ -1,5 +1,13 @@
 package com.Nalecy.www.dao;
 
+import com.Nalecy.www.util.DatabaseUtil;
+
+import java.util.LinkedList;
+
 public class UserDao {
-    
+    public static String getPassword(String userName){
+        LinkedList<String> password = DatabaseUtil.getInstance().getOneColumnData("user","password","username",userName);
+        if(password == null)return null;
+        return password.get(0);
+    }
 }
