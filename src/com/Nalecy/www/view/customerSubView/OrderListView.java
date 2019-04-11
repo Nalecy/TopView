ea137872a15c6a4c8c1517798571ef0e81e87d16
Nalecy <656177313@qq.com>
2019-04-12 01:49:09 +0800

    feat(view) :完成了顾客订单列表的视图

--- src/com/Nalecy/www/view/customerSubView/OrderListView.java
+++ src/com/Nalecy/www/view/customerSubView/OrderListView.java
@@ -1,9 +1,14 @@
+
 package com.Nalecy.www.view.customerSubView;
 
 import com.Nalecy.www.po.Hotel;
 import com.Nalecy.www.po.Order;
+import com.Nalecy.www.po.forTableView.OrderT;
+import com.Nalecy.www.service.OrderService;
 import com.Nalecy.www.util.TableViewCreater;
+import com.Nalecy.www.view.CustomerView;
 import com.Nalecy.www.view.alert.PromptAlert;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
@@ -17,6 +22,7 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.sql.Date;
+import java.util.ArrayList;
 
 public class OrderListView {
     private static OrderListView instance;
@@ -33,10 +39,10 @@ public class OrderListView {
     private HBox hBox;
 
     private Label finOrderLabel;
-    private TableView<Order> finTableView;
+    private TableView<OrderT> finTableView;
 
     private Label unfinOrderLabel;
-    private TableView<Order> unfinTableView;
+    private TableView<OrderT> unfinTableView;
 
     private Label tipLabel;
     private Button cancelOrderButton;
@@ -44,16 +50,40 @@ public class OrderListView {
 
     public void display(){
         init();
+        setButtonAction();
         window.show();
     }
+
+    private void setButtonAction() {
+        backButton.setOnAction(e -> {
+            CustomerView.getInstance().display();
+            window.close();
+        });
+        cancelOrderButton.setOnAction(e -> {
+            cancelOrder();
+        });
+
+    }
+
+    private void cancelOrder() {
+        Order order = unfinTableView.getSelectionModel().getSelectedItem().getOrder();
+        if(order == null){
+            PromptAlert.display("错误","未选择未完成订单");
+        }else {
+            OrderService.getInstance().cancelOrder(order);
+        }
+
+    }
+
     private void init(){
         finOrderLabel = new Label("已完成订单：");
 
-        TableViewCreater<Order> tvc = new TableViewCreater<>();
+        TableViewCreater<OrderT> tvc = new TableViewCreater<>();
         tvc.addStringColumn("用户名","userName",100);
         tvc.addDateColumn("订单日期","date",100);
-        tvc.addIntegerColumn("房间时段","roomPeriod",100);
-        tvc.addIntegerColumn("酒店id","hotelID",100);
+        tvc.addIntegerColumn("房间时段","roomTime",100);
+        tvc.addStringColumn("酒店名字","hotelName",100);
+        tvc.addStringColumn("房间名字","roomName",100);
         finTableView = tvc.getTableView();
         finTableView.setItems(getFOrderList());
 
@@ -61,8 +91,9 @@ public class OrderListView {
         tvc = new TableViewCreater<>();
         tvc.addStringColumn("用户名","userName",100);
         tvc.addDateColumn("订单日期","date",100);
-        tvc.addIntegerColumn("房间时段","roomPeriod",100);
-        tvc.addIntegerColumn("酒店id","hotelID",100);
+        tvc.addIntegerColumn("房间时段","roomTime",100);
+        tvc.addStringColumn("酒店名字","hotelName",100);
+        tvc.addStringColumn("房间名字","roomName",100);
         unfinTableView = tvc.getTableView();
         unfinTableView.setItems(getUFOrderList());
 
@@ -72,9 +103,11 @@ public class OrderListView {
         backButton = new Button("返回");
         backButton.setMinWidth(100);
 
+        tipLabel = new Label("您可以：");
+
         hBox = new HBox();
         hBox.setSpacing(50);
-        hBox.getChildren().addAll(cancelOrderButton,backButton);
+        hBox.getChildren().addAll(tipLabel,cancelOrderButton,backButton);
 
         vBox = new VBox();
         vBox.setSpacing(10);
@@ -88,11 +121,21 @@ public class OrderListView {
         window.setTitle("查看订单");
     }
 
-    private ObservableList<Order> getUFOrderList() {
-        return null;
+    private ObservableList<OrderT> getUFOrderList() {
+        ObservableList<OrderT> orders = FXCollections.observableArrayList();
+        ArrayList<Order> orderList = OrderService.getInstance().getIncompleteOrder();
+        for (Order order : orderList) {
+            orders.add(new OrderT(order));
+        }
+        return orders;
     }
 
-    private ObservableList<Order> getFOrderList() {
-        return null;
+    private ObservableList<OrderT> getFOrderList() {
+        ObservableList<OrderT> orders = FXCollections.observableArrayList();
+        ArrayList<Order> orderList = OrderService.getInstance().getCompleteOrder();
+        for (Order order : orderList) {
+            orders.add(new OrderT(order));
+        }
+        return orders;
     }
 }
