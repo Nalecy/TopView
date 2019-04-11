4eb2f90b300ca085d0c1971798094c07d2b227c2
Nalecy <656177313@qq.com>
2019-04-12 01:44:53 +0800

    feat(dao) : 完成了基本的增删查改功能

---
+++ src/com/Nalecy/www/dao/AdministratorDao.java
@@ -0,0 +1,49 @@
+package com.Nalecy.www.dao;
+
+import com.Nalecy.www.po.Administrator;
+import com.Nalecy.www.po.Person;
+import com.Nalecy.www.util.DatabaseUtil;
+
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+
+public class AdministratorDao {
+    public static void addAdministrator(Person person) {
+        Administrator administrator = (Administrator)person;
+        DatabaseUtil.addOneRowData("administrator", String.valueOf(administrator.getId()), administrator.getUserName(), administrator.getPassword(), administrator.getIdNumber(), administrator.getTelephone());
+    }
+
+    public static Administrator searchAdministrator(String userName) {
+        Administrator administrator = new Administrator();
+        LinkedList<String> list = DatabaseUtil.getOneRowData("administrator", "userName", userName);
+        if (list == null) return null;
+        setInfo(administrator,list);
+        return administrator;
+    }
+
+    public static Administrator searchAdministrator(Integer id) {
+        Administrator administrator = new Administrator();
+        LinkedList<String> list = DatabaseUtil.getOneRowData("administrator", "id", String.valueOf(id));
+        if (list == null) return null;
+        setInfo(administrator,list);
+        return administrator;
+    }
+
+    private static void setInfo(Administrator administrator, List<String> list) {
+        administrator.setId(Integer.valueOf(list.get(0)));
+        administrator.setUserName(list.get(1));
+        administrator.setPassword(list.get(2));
+        administrator.setIdNumber(list.get(3));
+        administrator.setTelephone(list.get(4));
+    }
+
+    public static void updateAdministrator(Administrator administrator) {
+        LinkedHashMap<String, String> lhm = new LinkedHashMap<>();
+        lhm.put("userName", administrator.getUserName());
+        lhm.put("password", administrator.getPassword());
+        lhm.put("idNumber", administrator.getIdNumber());
+        lhm.put("telephone", administrator.getTelephone());
+        DatabaseUtil.updateRowsData("administrator", "id", String.valueOf(administrator.getId()), lhm);
+    }
+}
--- src/com/Nalecy/www/dao/UserDao.java
+++ src/com/Nalecy/www/dao/UserDao.java
@@ -1,13 +1,46 @@
 package com.Nalecy.www.dao;
 
+import com.Nalecy.www.po.User;
 import com.Nalecy.www.util.DatabaseUtil;
 
+import java.util.LinkedHashMap;
 import java.util.LinkedList;
+import java.util.List;
 
 public class UserDao {
-    public static String getPassword(String userName){
-        LinkedList<String> password = DatabaseUtil.getInstance().getOneColumnData("user","password","username",userName);
-        if(password == null)return null;
-        return password.get(0);
+
+    public static boolean hasUser(String userName) {
+        List<String> userList = DatabaseUtil.getOneColumnData("user", "userName");
+        assert userList != null;
+        for (String s : userList) {
+            if (userName.equals(s)) return true;
+        }
+        return false;
     }
+
+    public static void addUser(User user) {
+        DatabaseUtil.addOneRowData("user", null, user.getUserName(), user.getPassword());
+    }
+
+    public static User getUser(String userName) {
+        User user = new User();
+        LinkedList<String> userInfo = DatabaseUtil.getOneRowData("user", "userName", userName);
+        if (userInfo == null) return null;
+        user.setId(Integer.valueOf(userInfo.get(0)));
+        user.setUserName(userInfo.get(1));
+        user.setPassword(userInfo.get(2));
+        return user;
+    }
+
+    public static void deleteUser(String userName) {
+        DatabaseUtil.deleteOneRowData("user", "userName", userName);
+    }
+
+    public static void updateUser(String userName, String updPassword) {
+        LinkedHashMap<String, String> lhm = new LinkedHashMap<>();
+        lhm.put("password", updPassword);
+        DatabaseUtil.updateRowsData("user", "userName", userName, lhm);
+    }
+
+
 }
