88d618d35d34a552dc7d4c9957fd486d31394f1b
Nalecy <656177313@qq.com>
2019-04-14 19:58:11 +0800

    feat(view) :新增了可用于添加或修改的工具弹窗

---
+++ src/com/Nalecy/www/view/popupUtil/InfoEditPopup.java
@@ -0,0 +1,122 @@
+package com.Nalecy.www.view.popupUtil;
+
+import com.Nalecy.www.po.Room;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.lang.reflect.Array;
+import java.util.*;
+
+public class InfoEditPopup {
+    private boolean aBoolean; //用于返回是否确定
+
+    private List<String> returnedDataList = null;
+    private List<String> infoNameList = null;
+    private List<String> infoValueList;
+    private HashMap<Label, TextField> labelWithTextF;
+
+    private Stage stage;
+    private Scene scene;
+
+    private VBox vBox;
+
+    private HBox editHBox;
+    private VBox leftVBox;
+    private VBox rightVBox;
+
+    private HBox buttonHBox;
+    private Button confirmButton;
+    private Button cancelButton;
+
+
+    public List<String> display() {
+        init();
+        setButtonAction();
+
+        stage.showAndWait();
+        return returnedDataList;
+    }
+
+    private void setButtonAction() {
+        cancelButton.setOnAction(e -> {
+            returnedDataList = null;
+            stage.close();
+        });
+        confirmButton.setOnAction(e -> {
+            returnedDataList = new ArrayList<>();
+            for (Label key : labelWithTextF.keySet()) {
+                String value = labelWithTextF.get(key).getText();
+                if (value == null || value.length() == 0) {
+                    PromptAlert.display("错误", "请不要留空");
+                    return;
+                }
+                returnedDataList.add(value);
+            }
+            stage.close();
+        });
+    }
+
+    private void init() {
+        initEditField();//初始化编辑区域布局
+
+        //初始化按钮布局
+        cancelButton = new Button("返回");
+        confirmButton = new Button("确定");
+
+        buttonHBox = new HBox();
+        buttonHBox.getChildren().addAll(cancelButton, confirmButton);
+        buttonHBox.setSpacing(50);
+        buttonHBox.setAlignment(Pos.CENTER);
+        //初始化总布局
+        vBox = new VBox();
+        vBox.getChildren().addAll(editHBox, buttonHBox);
+        vBox.setSpacing(10);
+        vBox.setPadding(new Insets(10));
+
+        scene = new Scene(vBox);
+        stage = new Stage();
+        stage.setScene(scene);
+        stage.initModality(Modality.APPLICATION_MODAL);
+    }
+
+    private void initEditField() {
+        int dataNumber = infoNameList.size();
+        labelWithTextF = new LinkedHashMap<>();
+        leftVBox = new VBox();
+        rightVBox = new VBox();
+        for (int i = 0; i < dataNumber; i++) {
+            Label label = new Label(infoNameList.get(i) + ":");//
+            TextField text = new TextField();
+            if (infoValueList != null) text.setText(infoValueList.get(i));//如果未设置值列表，则不进行编辑框赋值
+            labelWithTextF.put(label, text);
+
+            leftVBox.getChildren().add(label);
+            rightVBox.getChildren().add(text);
+        }
+        leftVBox.setSpacing(21);
+        rightVBox.setSpacing(10);
+        editHBox = new HBox();
+        editHBox.getChildren().addAll(leftVBox, rightVBox);
+        editHBox.setPadding(new Insets(10));
+    }
+
+
+    public void setInfoNameList(String...infoName) {
+        infoNameList = new ArrayList<>();
+        infoNameList.addAll(Arrays.asList(infoName));
+    }
+    public void setInfoValueList(String...valueName){
+        infoValueList = new ArrayList<>();
+        infoValueList.addAll(Arrays.asList(valueName));
+    }
+
+
+}
