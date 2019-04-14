df26e3229dab06574f4036afd2090edf34a2b083
Nalecy <656177313@qq.com>
2019-04-15 01:37:44 +0800

    no message

--- src/com/Nalecy/www/discardMenu/hotelAdminMenu/PsnlInfoMenu.java
+++ src/com/Nalecy/www/discardMenu/hotelAdminMenu/PsnlInfoMenu.java
@@ -16,7 +16,7 @@ public class PsnlInfoMenu extends Menu {//废弃
     @Override
     public void show() {
 
-        System.out.println("您好，名为"+customer.getUserName()+"的酒店管理员，下面是您的信息：");
+        /*System.out.println("您好，名为"+customer.getUserName()+"的酒店管理员，下面是您的信息：");
         printNowInfo();
         System.out.println("您想要修改(输入1保存，0返回)：");
         while(true){
@@ -48,7 +48,7 @@ public class PsnlInfoMenu extends Menu {//废弃
             }else System.out.println("请检查输入");
             printNowInfo();
 
-        }
+        }*/
     }
 
     private void printNowInfo(){
