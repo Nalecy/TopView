8578297343044ff1e3611b48c82fd15b324a421b
Nalecy <656177313@qq.com>
2019-04-16 22:06:46 +0800

    feat(util) :新增了快速增加提示文本的工具类

---
+++ src/com/Nalecy/www/util/LabelsCreater.java
@@ -0,0 +1,26 @@
+package com.Nalecy.www.util;
+
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+
+public class LabelsCreater {
+    private VBox vBox;
+    public LabelsCreater(){
+        vBox = new VBox();
+        vBox.setSpacing(10);
+    }
+    public VBox getVBox(){
+        return vBox;
+    }
+    public void addLine(String labelString){
+        vBox.getChildren().add(new Label(labelString));
+    }
+    public void setLine(Integer index,String labelString){
+        Label label= (Label) vBox.getChildren().get(index);
+        label.setText(labelString);
+    }
+    public void deleteLine(Integer index){
+        vBox.getChildren().remove(index.intValue());
+    }
+
+}
